d9c419002f8bafea15edc2ae6d4283a47081b989
fengyu <windtalker03@gmail.com>
2012-05-23 23:00:06 +1000

    Wednesday group meeting

---
+++ VideoStoreGroupProject/VideoStoreGroupProjectS12012/Bank.Services/Bank.Services.Interfaces/ITransferService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ServiceModel;
+
+namespace Bank.Services.Interfaces
+{
+    [ServiceContract]
+    public interface ITransferService
+    {
+        //[OperationContract]
+        [OperationContract(IsOneWay = true)]
+        [TransactionFlow(TransactionFlowOption.Allowed)]
+        void Transfer(decimal pAmount, int pFromAcctNumber, int pToAcctNumber, String pDescription);
+    }
+}
